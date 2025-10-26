8bd77905dcab6181e9858735181c8fbccc14436b
I529023 <kshitijachauhan22@gmail.com>
2025-10-26 23:07:22 +0530

    springInitial + postgres db setup using docker

---
+++ src/test/java/com/vrms/ngo_posting_service/NgoPostingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrms.ngo_posting_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoPostingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
